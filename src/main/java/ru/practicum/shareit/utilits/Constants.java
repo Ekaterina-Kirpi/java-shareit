91b6cd36c016c5facb0a23b020c66aeeab712dee
Kirpi <kirpicheva89@yandex.ru>
2023-08-05 02:17:06 +0300

    style

--- src/main/java/ru/practicum/shareit/utilits/Constants.java
+++ src/main/java/ru/practicum/shareit/utilits/Constants.java
@@ -1,5 +1,8 @@
 package ru.practicum.shareit.utilits;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Constants {
     public static final String USER_ID_HEADER = "X-Sharer-User-Id";
     public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS";
