5638865367896eaf1ca37b178a1c2611d91fe607
Kirpi <kirpicheva89@yandex.ru>
2023-08-10 14:35:16 +0300

    ref

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,12 +1,16 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
     @Size(max = 255)
     @NotBlank(message = "Нужно представиться :)")
