f0bae0f613db561729289338b74f4e3c45786f01
Kirpi <kirpicheva89@yandex.ru>
2023-08-11 12:08:10 +0300

    ref

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -13,7 +13,6 @@ import ru.practicum.shareit.request.dto.RequestDtoResponse;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
